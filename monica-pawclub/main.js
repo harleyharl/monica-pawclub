954f8df939b33e54362c62f0cf9e3b0ef5fbb1a5
EugeneRaynerNZ <rayner.euge@gmail.com>
2018-04-12 13:44:37 +1200

    fullpage commit

---
+++ monica-pawclub/main.js
@@ -0,0 +1,11 @@
+// smooth scroll on link click
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
